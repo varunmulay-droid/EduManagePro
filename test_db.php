27c213198c90848d880c70fd93f49550acba5c87
Varun Vinayak Mulay <sullyjake699@gmail.com>
2025-08-01 00:24:12 +0530

    Update test_db.php

--- test_db.php
+++ test_db.php
@@ -1,4 +1,19 @@
 <?php
+
+$host = getenv("DB_HOST");
+$user = getenv("DB_USER");
+$pass = getenv("DB_PASS");
+$db   = getenv("DB_NAME");
+$port = 3306;  // required for db4free.net
+
+$conn = new mysqli($host, $user, $pass, $db, $port);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+
 require_once 'config/config.php';
 require_once 'config/database.php';
 
